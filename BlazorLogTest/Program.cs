400feb9c8a2a3a92f6f9ac7363e286ab8333ca3e
AlexNek <alexnek@russinger.com>
2023-08-03 23:13:11 +0200

    add blazor log test

---
+++ BlazorLogTest/Program.cs
@@ -0,0 +1,50 @@
+using BlazorLogTest.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Serilog;
+
+namespace BlazorLogTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            builder.Host.UseSerilog((hostContext, services, loggerConfiguration) =>
+                {
+                    loggerConfiguration.ReadFrom.Configuration(builder.Configuration);
+                    //loggerConfiguration.WriteTo.Console(outputTemplate: "{Timestamp:HH:mm:ss.fff} [{Level:u3} {ThreadId:d2}] {Message:lj} <{SourceContext}>{NewLine}{Exception}");
+                    //loggerConfiguration.Enrich.FromLogContext();
+                    //loggerConfiguration.Enrich.WithThreadId();
+                });
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
